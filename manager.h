580d2e0b01082dfceb336188a65b0a2310b366fb
Kim Koeun <22000050@handong.edu>
2021-05-07 09:24:59 +0900

    create manager.h

---
+++ manager.h
@@ -0,0 +1,13 @@
+#ifndef _MANAGER_H_
+
+#define _MANAGER_H_
+#include <stdio.h>
+#include "product.h"
+
+void listClass(Class *c, int count);
+int selectDataNo(Class *c, int count);
+void searchClass(Class *c, int count);
+void searchProf(Class *c, int count);
+void searchCredit(Class *c, int count);
+
+#endif
