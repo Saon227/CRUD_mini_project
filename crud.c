2306170712b414e76d67454009ded39f892a3cee
sijin0 <you@example.com>
2021-05-08 12:32:53 +0900

    create addClass

--- crud.c
+++ crud.c
@@ -1,8 +1,17 @@
 #include "crud.h"
 
 int addClass(Class *c){
-
+    printf("수업의 이름은? ");
+    scanf("%s",c->name);
+    printf("교수님 성함은? ");
+    scanf("%s", c->profname);
+    printf("학점은? ");
+    scanf("%f", &c->credit);
+    printf("교시는? ");
+    scanf("%d", &c->period);
+    return 1;
 }
+
 void readClass(Class c){
 
 }
