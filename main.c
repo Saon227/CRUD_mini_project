7e754fdf8f3ab8c504ab3654adb06c325e314e3e
Kim Koeun <22000050@handong.edu>
2021-05-09 15:38:22 +0900

    create deleteClass, listClass and selectClass

--- main.c
+++ main.c
@@ -3,7 +3,27 @@
 #include <stdio.h>
 
 int main(){
+	int count = 0;
+	int result = 0;
+	Class clist[20];
+	int index =0;
 
+	count += addClass(&clist[index++]);
+	printf("번호 수업 교수님 학점 교시\n");
 
+	for(int i =0; i <count ; i++){
+		if(clist[i].credit == 0) continue;
+		printf("%2d ", i+1);
+		readClass(clist[i]);
+	}
+
+	printf("\n");
+	updateClass(&clist[index-1]);
+
+	for(int i =0; i <count ; i++){
+		if(clist[i].credit == 0) continue;
+		printf("%2d ", i+1);
+		readClass(clist[i]);
+	}
 	return 0;
 }
--- manager.c
+++ manager.c
@@ -2,10 +2,18 @@
 #include "manager.h"
 
 void listClass(Class *c, int count){
-
-
+    printf("Name Professor Credit Period\n================================\n");
+    for(int i=0; i<count; i++){
+        if(c[i].credit == -1) continue;
+	printf("%2d ", i+1);
+	readClass(c[i]);
+    }
 }
+
 int selectDataNo(Class *c, int count){
-
-
+    listClass(c, count);
+    int num;
+    printf("\n번호는 (취소 :0)? ");
+    scanf("%d", &num);
+    return num;
 }
