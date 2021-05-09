bc9769c6ac49ec056ebdc3b16d9b1cc6f8283c3c
Kim Koeun <22000050@handong.edu>
2021-05-09 16:13:23 +0900

    crud.c 수정

--- crud.c
+++ crud.c
@@ -1,27 +1,28 @@
 #include "crud.h"
 
 int addClass(Class *c){
-    printf("수업의 이름은? ");
-    scanf("%s",c->name);
+    printf("\n수업의 이름은? ");
+    scanf("%[^\n]s",c->name);
     printf("교수님 성함은? ");
-    scanf("%s", c->profname);
+    scanf("%[^\n]s", c->profname);
     printf("학점은? ");
     scanf("%f", &c->credit);
     printf("교시는? ");
     scanf("%d", &c->period);
+    printf("==> 추가됨\n");
     return 1;
 }
 
 void readClass(Class c){
-    printf("%8s %8s %.1f %4d\n",
+    printf("%-8s %8s %.1f %4d\n",
     c.name, c.profname, c.credit, c.period);
 }
 
 int updateClass(Class *c){
     printf("수업의 이름은? ");
-    scanf("%s",c->name);
+    scanf("%[^\n]s",c->name);
     printf("교수님 섬함은? ");
-    scanf("%s", c->profname);
+    scanf("%[^\n]s", c->profname);
     printf("학점은? ");
     scanf("%f", &c->credit);
     printf("교시는? ");
