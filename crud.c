01b379b365fbd29c9d2b4c4629fb035534b9afe6
Kim Koeun <22000050@handong.edu>
2021-05-09 16:32:05 +0900

    에러 수정

--- crud.c
+++ crud.c
@@ -2,32 +2,36 @@
 
 int addClass(Class *c){
     printf("\n수업의 이름은? ");
+    getchar();
     scanf("%[^\n]s",c->name);
     printf("교수님 성함은? ");
+    getchar();
     scanf("%[^\n]s", c->profname);
     printf("학점은? ");
     scanf("%f", &c->credit);
     printf("교시는? ");
     scanf("%d", &c->period);
-    printf("==> 추가됨\n");
+    printf("추가됨!\n");
     return 1;
 }
 
 void readClass(Class c){
-    printf("%-8s %8s %.1f %4d\n",
+    printf("%-15s %12s %8.1f %4d\n",
     c.name, c.profname, c.credit, c.period);
 }
 
 int updateClass(Class *c){
     printf("수업의 이름은? ");
+    getchar();
     scanf("%[^\n]s",c->name);
     printf("교수님 섬함은? ");
+    getchar();
     scanf("%[^\n]s", c->profname);
     printf("학점은? ");
     scanf("%f", &c->credit);
     printf("교시는? ");
     scanf("%d", &c->period);
-    printf("=> 수정됨!\n");
+    printf("수정됨!\n");
     return 1;
 }
 
--- manager.c
+++ manager.c
@@ -2,7 +2,7 @@
 #include "manager.h"
 
 void listClass(Class *c, int count){
-    printf("No Name     Professor Credit Period\n================================\n");
+    printf("No ClassName       Professor Credit Period\n========================================\n");
     for(int i=0; i<count; i++){
         if(c[i].credit == -1) continue;
 	printf("%2d ", i+1);
