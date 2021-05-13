bc71efd95469668df2982e79f06d4bfb461d3951
Kim Koeun <22000050@handong.edu>
2021-05-13 22:02:32 +0900

    data.c 수정

--- data.c
+++ data.c
@@ -6,7 +6,7 @@ void saveData(Class *c, int count){
     fp = fopen("data.txt", "wt");
     for(int i=0; i<count; i++){
         if(c[i].credit == -1) continue;
-        fprintf(fp, "%s %s %.1f %d\n", c[i].name, c[i].profname, c[i].credit, c[i].period);
+        fprintf(fp, "%s\n%s\n%.1f %d\n", c[i].name, c[i].profname, c[i].credit, c[i].period);
     }
     fclose(fp);
     printf("=> 저장됨!\n");
@@ -17,9 +17,11 @@ int loadData(Class *c){
         int i;
         for(i=0; i<100; i++){
             fscanf(fp, "%[^\n]s", c[i].name);
+	    getchar();
             if(feof(fp)) break;
             fscanf(fp, "%[^\n]s", c[i].profname);
-            fscanf(fp, "%f", &c[i].credit);
+            getchar();
+	    fscanf(fp, "%f", &c[i].credit);
             fscanf(fp, "%d", &c[i].period);
         }
         printf("=> 로딩 성공!\n");
