0bfaf3de1763692a5b8f96f867678a8613afab07
Kim Koeun <22000050@handong.edu>
2021-05-13 21:33:31 +0900

    create loadData

--- data.c
+++ data.c
@@ -2,7 +2,14 @@
 #include "data.h"
 
 void saveData(Class *c, int count){
-
+    FILE *fp;
+    fp = fopen("data.txt", "wt");
+    for(int i=0; i<count; i++){
+        if(c[i].score == -1) continue;
+        fprintf(fp, "%s %s %.1f %d\n", c[i].name, c[i].profname, c[i].credit, c[i].period);
+    }
+    fclose(fp);
+    printf("=> 저장됨!\n");
 }
 int loadData(Class *c){
 
